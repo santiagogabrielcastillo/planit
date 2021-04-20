f86ef4878ac8e8839612d7bb9e0deda531efadbd
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-04-20 14:22:14 -0300

    Changes seeds and adds Provider model validation for category

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,3 +1,9 @@
+CATEGORIES = ["Tutorial", "Pastelería", "Foodtrucks", "Barras de tragos", "Salones",
+              "Fotografía", "Bebidas", "Música", "Cotillón", "Animación", "Mozos", "Vajilla",
+              "Seguridad", "Mobiliario", "Catering"]
+
 class Provider < ApplicationRecord
   has_many :services
+
+  validates :category, inclusion: { in: CATEGORIES }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,12 @@ puts "Cleaning data..."
 
 Service.destroy_all
 Provider.destroy_all
-Category.destroy_all
 
 puts "Creating data..."
 
-CATEGORIES = ["Tutorial", "Pastelería", "Foodtrucks", "Barras de tragos", "Salones", "Fotografía", "Bebidas", "Música", "Cotillón", "Animación", "Mozos", "Vajilla", "Seguridad", "Mobiliario", "Catering"]
-
-CATEGORIES.each do |category|
-  Category.create!(name: category)
-end
-
 DELIVERY = ["Entrega a domicilio", "Retiro en el local"]
 
-Category.all.each do |category|
+CATEGORIES.each do |category|
   3.times do
     Provider.create!(
       name: Faker::Company.name,
