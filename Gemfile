3cda44f8b7707ba67b2c03cc8f5287d2feb91cb9
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-04-20 12:54:54 -0300

    adds seeds

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'devise'
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass'
 gem 'simple_form'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning data..."
+
+Service.destroy_all
+Provider.destroy_all
+Category.destroy_all
+
+puts "Creating data..."
+
+CATEGORIES = ["Tutorial", "Pastelería", "Foodtrucks", "Barras de tragos", "Salones", "Fotografía", "Bebidas", "Música", "Cotillón", "Animación", "Mozos", "Vajilla", "Seguridad", "Mobiliario", "Catering"]
+
+CATEGORIES.each do |category|
+  Category.create!(name: category)
+end
+
+DELIVERY = ["Entrega a domicilio", "Retiro en el local"]
+
+Category.all.each do |category|
+  3.times do
+    Provider.create!(
+      name: Faker::Company.name,
+      address: Faker::Address.street_address,
+      description: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 15),
+      delivery: DELIVERY.sample,
+      schedule: "#{rand(7..10)}:00 a #{rand(18..21)}:00",
+      category: category
+    )
+  end
+end
