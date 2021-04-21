d03ed98810861a9f1a4d4307cbb7eaf2415b4a62
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-04-21 13:17:40 -0300

    Delete autogenerated comments

--- app/admin/providers.rb
+++ app/admin/providers.rb
@@ -1,19 +1,6 @@
 ActiveAdmin.register Provider do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
   permit_params :name, :address, :description, :delivery, :schedule, category_ids: []
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :address, :description, :delivery, :schedule]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
 
   index do
     selectable_column
