4e519bbb1e16af74f651a370fbabc3b0bcca4054
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-04-26 12:04:54 -0300

    Adds users#edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,27 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %I[show edit update]
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
     @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :phone)
   end
 end
