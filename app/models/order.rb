6c3bd2768770b666e442fbcda895beef8be7a303
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-04-27 11:13:24 -0300

    Adds model Order validation for guests number

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,6 @@ DELIVERY = ["A domicilio", "Retiro en local"]
 class Order < ApplicationRecord
   belongs_to :service
   belongs_to :user
+
+  validates :guests, numericality: { greater_than_or_equal_to: 1 }
 end
