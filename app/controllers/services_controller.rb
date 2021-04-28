bdc25f18aa546c1da6ea9d1d37b45b4e08c3a854
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-04-28 08:49:32 -0300

    Adds photo attached as file for provider and service models

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,6 @@
 class ServicesController < ApplicationController
-  before_action :set_provider, only: %I[new create show]
+  before_action :set_provider, only: %I[new create show edit update]
+  before_action :set_service, only: %I[show edit update]
 
   def new
     @service = Service.new
@@ -16,17 +17,31 @@ class ServicesController < ApplicationController
   end
 
   def show
-    @service = Service.find(params[:id])
     @order = Order.new
   end
 
+  def edit
+  end
+
+  def update
+    if @service.update(service_params)
+      redirect_to provider_service_path(@provider, @service)
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_service
+    @service = Service.find(params[:id])
+  end
 
   def set_provider
     @provider = Provider.find(params[:provider_id])
   end
 
   def service_params
-    params.require(:service).permit(:name, :description, :cost, :provider_id)
+    params.require(:service).permit(:name, :description, :cost, :photo, :provider_id)
   end
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,4 +1,6 @@
 class Provider < ApplicationRecord
   has_many :services
   has_and_belongs_to_many :categories
+
+  has_one_attached :photo
 end
