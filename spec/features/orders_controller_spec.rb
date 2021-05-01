02609dc0ba623a0d9f98f78233298974fb777334
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-05-01 10:28:14 -0300

    Adds feature tests for Order controller

--- spec/features/orders_controller_spec.rb
+++ spec/features/orders_controller_spec.rb
@@ -8,19 +8,53 @@ RSpec.feature "OrdersControllers", type: :feature, js: true do
     create(:service)
     login_as(user, scope: :user)
     visit root_path
+    find('img.category-img').click
+    find('h3', text: 'La Pasionaria Cocina').find(:xpath, '..').click
+    find('h4', text: 'Brunch completo').click
   end
 
-  context 'with guests > 0' do
-    it 'generates a new order' do
-      visit '/'
-      find('img.category-img').click
-      find('h3', text: 'La Pasionaria Cocina').find(:xpath, '..').click
-      find('h4', text: 'Brunch completo').click
+  describe 'New order proccess' do
+    context 'with guests > 0' do
+      it 'generates a new order and redirect to the edit order page' do
+        click_button '+'
+        click_button 'Comprar'
+
+        expect(page).to have_content 'Tipo de entrega'
+        expect(page).to have_current_path(edit_order_path(Order.first.id))
+      end
+    end
+
+    context 'without selecting number of guests' do
+      it 'displays an error message' do
+        click_button 'Comprar'
+
+        expect(page).to have_content 'Guests must be greater than or equal to 1'
+      end
+    end
+  end
+
+  describe "Giving order's details" do
+    before(:each) do
       click_button '+'
       click_button 'Comprar'
+    end
 
-      # expect(page).to have_content 'Brunch completo'
-      expect(page).to have_content 'Tipo de entrega'
+    context 'when selecting home delivery and giving no address' do
+      it 'displays an error message' do
+        find('label', text: 'A domicilio').click
+        click_on 'Continuar'
+
+        expect(page).to have_content("Address can't be blank")
+      end
+    end
+
+    context 'when giving no date' do
+      it 'shows an error message' do
+        find('label', text: 'Retiro en local').click
+        click_on 'Continuar'
+
+        expect(page).to have_content("Date can't be blank")
+      end
     end
   end
 end
--- spec/features/users_session_controller_spec.rb
+++ spec/features/users_session_controller_spec.rb
@@ -1,33 +1,49 @@
 require 'rails_helper'
 
-RSpec.feature "UsersSessionControllers", type: :feature do
+RSpec.feature "UsersSessionControllers", type: :feature, js: true do
   let!(:user) { create(:user) }
 
   before(:each) do
     create(:category)
   end
 
-  context 'with valid credentials' do
-    it 'redirects logged in user to categories index' do
-      visit 'users/sign_in'
-      fill_in 'Email', with: user.email
-      fill_in 'Contraseña', with: user.password
-      click_button 'Registrarme'
+  describe 'sign in' do
+    context 'with valid credentials' do
+      it 'redirects logged in user to categories index' do
+        visit 'users/sign_in'
+        fill_in 'Email', with: user.email
+        fill_in 'Contraseña', with: user.password
+        click_button 'Registrarme'
 
-      expect(page).to have_current_path(root_path)
+        expect(page).to have_current_path(root_path)
+      end
+    end
+
+    context 'with invalid credentials' do
+      it 'shows an invalid information message and stays in the log in page' do
+        visit 'users/sign_in'
+        fill_in 'Email', with: user.email
+        fill_in 'Contraseña', with: 'contraseña'
+        click_button 'Registrarme'
+
+        expect(page).to have_content("Invalid Email or password")
+        expect(page).to have_current_path(new_user_session_path)
+      end
     end
   end
 
-  context 'with valid information' do
-    it 'creates a new user and redirect to categories index' do
-      visit 'users/sign_in'
-      click_link 'No tengo cuenta'
-      fill_in 'Nombre', with: "Nombre de ejemplo"
-      fill_in 'Email', with: "email@email.com"
-      fill_in 'Contraseña', with: "contraseña"
-      click_button 'Registrarme'
+  describe 'sign up' do
+    context 'with valid information' do
+      it 'creates a new user and redirect to categories index' do
+        visit 'users/sign_in'
+        click_link 'No tengo cuenta'
+        fill_in 'Nombre', with: "Nombre de ejemplo"
+        fill_in 'Email', with: "email@email.com"
+        fill_in 'Contraseña', with: "contraseña"
+        click_button 'Registrarme'
 
-      expect(page).to have_current_path(root_path)
+        expect(page).to have_current_path(root_path)
+      end
     end
   end
 end
